c4c1c769b2707b8dd7e726f5fc7a16b116bc565a
maximvss <32554246+maximvss@users.noreply.github.com>
2024-08-12 21:55:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('myButton').addEventListener('click', function() {
+      alert('¡Botón clickeado!');
+    });
+  });
+  
